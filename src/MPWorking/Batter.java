eeed6805bfd8f1a7ea6e28b128fe47a699fbd8f1
itswin <winston.cheung1@gmail.com>
2023-09-24 16:37:05 -0500

    Batters rotate through targets

--- src/MPWorking/Batter.java
+++ src/MPWorking/Batter.java
@@ -5,13 +5,39 @@ import java.util.function.Predicate;
 import aic2023.user.*;
 
 public class Batter extends Robot {
+    public int commsBaseIndex;
+    public int commsStadiumIndex;
+
+    final class TargetTypeEnum {
+        public final int BASE = 0;
+        public final int STADIUM = 1;
+        public final int EXPLORE = 2;
+    }
+
+    public TargetTypeEnum TargetType = new TargetTypeEnum();
+
+    public int targetType;
+    public Location target;
+    public boolean justStartedExploring;
+
     public Batter(UnitController u) {
         super(u);
+
+        targetType = TargetType.BASE;
+        commsBaseIndex = 0;
+        commsStadiumIndex = 0;
+        targetType = TargetType.BASE;
+        target = null;
+        justStartedExploring = false;
     }
 
     public void initTurn() {
         super.initTurn();
         comms.incrementBatters();
+
+        if (shouldLoadNextTarget()) {
+            loadNextTarget();
+        }
     }
 
     public void takeTurn() {
@@ -31,15 +57,16 @@ public class Batter extends Robot {
             }
             return true;
         };
-        Location target;
+        Location visibleTarget = null;
         boolean isExploring = false;
-        if ((target = getClosestMapObj(MapObject.BASE, availablePred)) != null) {
-            comms.logBase(target);
-        } else if ((target = getClosestMapObj(MapObject.STADIUM, availablePred)) != null) {
-            comms.logStadium(target);
-        } else {
-            target = explore.getExplore3Target();
-            isExploring = true;
+        if ((visibleTarget = getClosestMapObj(MapObject.BASE, availablePred)) != null) {
+            comms.logBase(visibleTarget);
+        } else if ((visibleTarget = getClosestMapObj(MapObject.STADIUM, availablePred)) != null) {
+            comms.logStadium(visibleTarget);
+        }
+
+        if (visibleTarget != null) {
+            target = visibleTarget;
         }
 
         // If we're targeting a base/stadium and not all of the squares around
@@ -77,6 +104,56 @@ public class Batter extends Robot {
                     uc.bat(dir, strength);
                     debug.println("Batting: " + dir + " " + strength);
                     break;
+                }
+            }
+        }
+    }
+
+    public void rotateTargetType() {
+        if (targetType == TargetType.BASE) {
+            targetType = TargetType.STADIUM;
+        } else if (targetType == TargetType.STADIUM) {
+            targetType = TargetType.EXPLORE;
+            justStartedExploring = true;
+        } else if (targetType == TargetType.EXPLORE) {
+            targetType = TargetType.BASE;
+        }
+    }
+
+    public boolean shouldLoadNextTarget() {
+        // Always load next target if exploring
+        return target == null ||
+                uc.getLocation().distanceSquared(target) <= VISION_RANGE ||
+                targetType == TargetType.EXPLORE;
+    }
+
+    public void loadNextTarget() {
+        if (targetType == TargetType.BASE) {
+            if (commsBaseIndex >= comms.BASE_SLOTS ||
+                    (target = comms.readBase(commsBaseIndex++)).x == -1) {
+                commsBaseIndex = 0;
+                rotateTargetType();
+                loadNextTarget();
+            }
+        } else if (targetType == TargetType.STADIUM) {
+            if (commsStadiumIndex >= comms.STADIUM_SLOTS ||
+                    (target = comms.readStadium(commsStadiumIndex++)).x == -1) {
+                rotateTargetType();
+                loadNextTarget();
+                commsStadiumIndex = 0;
+            }
+        } else if (targetType == TargetType.EXPLORE) {
+            if (justStartedExploring) {
+                target = explore.getExplore3Target();
+                justStartedExploring = false;
+            } else {
+                Location newTarget = explore.getExplore3Target();
+                // If we reset the explore target, rotate target types
+                if (!target.equals(newTarget)) {
+                    rotateTargetType();
+                    loadNextTarget();
+                } else {
+                    target = newTarget;
                 }
             }
         }
