cf92953e7a59e8ea99b10fd6dc3bc892cc9b7f79
itswin <winston.cheung1@gmail.com>
2023-09-24 22:17:54 -0500

    Adjust init and batter/pitcher ratio

--- src/MPWorking/Hq.java
+++ src/MPWorking/Hq.java
@@ -3,6 +3,19 @@ package MPWorking;
 import aic2023.user.*;
 
 public class Hq extends Robot {
+    final class HqStateEnum {
+        public final int INIT = 0;
+        public final int NORMAL = 1;
+    }
+
+    public final int NUM_INIT_BATTERS = 1;
+    public final int NUM_INIT_PITCHERS = 1;
+
+    public final float BATTERS_TO_PITCHERS_RATIO = 2;
+
+    public HqStateEnum HqState = new HqStateEnum();
+    public int state;
+
     public int nextFlag;
 
     public Direction[] exploreDirections;
@@ -16,6 +29,7 @@ public class Hq extends Robot {
         super(u);
         comms.init();
 
+        state = HqState.INIT;
         nextFlag = 0;
         exploreDirections = explore.getOptimalExploreOrder();
         exploreDirIndex = 0;
@@ -33,20 +47,60 @@ public class Hq extends Robot {
     public void takeTurn() {
         super.takeTurn();
 
-        Direction dir = exploreDirections[exploreDirIndex++ % exploreDirections.length];
+        switchState();
+        doStateAction();
+
+        comms.writeHqFlag(nextFlag);
+        nextFlag = 0;
+    }
+
+    public void switchState() {
+        if (state == HqState.INIT) {
+            if (numBatters >= NUM_INIT_BATTERS && numPitchers >= NUM_INIT_PITCHERS) {
+                state = HqState.NORMAL;
+            }
+        }
+    }
+
+    public void doStateAction() {
+        if (state == HqState.INIT) {
+            initAction();
+        } else if (state == HqState.NORMAL) {
+            normalAction();
+        }
+    }
+
+    public void initAction() {
         UnitType recruitType;
-        if (numPitchers < 2 * numBatters + 5) {
+        if (numBatters < NUM_INIT_BATTERS) {
+            recruitType = UnitType.BATTER;
+        } else {
+            recruitType = UnitType.PITCHER;
+        }
+
+        Direction dir = exploreDirections[exploreDirIndex];
+        if (uc.canRecruitUnit(recruitType, dir)) {
+            uc.recruitUnit(recruitType, dir);
+            exploreDirIndex++;
+            exploreDirIndex %= exploreDirections.length;
+            nextFlag = util.dirToFlag(dir);
+        }
+    }
+
+    public void normalAction() {
+        UnitType recruitType;
+        if (numPitchers * BATTERS_TO_PITCHERS_RATIO < numBatters) {
             recruitType = UnitType.PITCHER;
         } else {
             recruitType = UnitType.BATTER;
         }
 
+        Direction dir = exploreDirections[exploreDirIndex];
         if (uc.canRecruitUnit(recruitType, dir)) {
             uc.recruitUnit(recruitType, dir);
+            exploreDirIndex++;
+            exploreDirIndex %= exploreDirections.length;
             nextFlag = util.dirToFlag(dir);
         }
-
-        comms.writeHqFlag(nextFlag);
-        nextFlag = 0;
     }
 }
